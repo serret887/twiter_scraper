293308e90cf36a547a35ad73a205ae39881eadd5
serret887 <serret887@gmail.com>
2018-01-13 20:28:44 +0000

    Adding the Tweet and the User items

--- twiter_scraper/items.py
+++ twiter_scraper/items.py
@@ -5,10 +5,44 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Field, Item
 
 
-class TwiterScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Tweet(Item):
+    iD = Field()  # tweet id
+    url = Field()  # tweet url
+    created_date = Field()  # post time
+    text = Field()  # text content
+    user_id = Field()  # user id
+    # TODO: check that i can search users by userId
+    usernameTweet = Field()  # username of tweet
+
+    retweet_amount = Field()  # nbr of retweet
+    like_amount = Field()  # nbr of favorite
+    reply_amount = Field()  # nbr of reply
+
+    is_reply = Field()  # boolean if the tweet is a reply or not
+    is_retweet = Field(
+    )  # boolean if the tweet is just a retweet of another tweet
+
+    has_image = Field()  # True/False, whether a tweet contains images
+    images = Field()  # a list of image urls, empty if none
+
+    has_video = Field()  # True/False, whether a tweet contains videos
+    videos = Field()  # a list of video urls
+
+    has_media = Field(
+    )  # True/False, whether a tweet contains media (e.g. summary)
+    medias = Field()  # a list of media
+
+
+class User(Item):
+    ID = Field()  # user id
+    name = Field()  # user name
+    screen_name = Field()  # user screen name
+    avatar = Field()  # avator url
+
+    tweets_amount = Field()
+    following_amount = Field()
+    follower_amount = Field()
+    like_amount = Field()
